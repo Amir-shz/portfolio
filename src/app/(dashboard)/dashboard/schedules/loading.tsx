695104a7d251b3abf4155690e7f0920409f39846
amir-shz <amirsharif2079@gmail.com>
2025-01-25 01:47:42 +0330

    fix full page loader size

--- src/app/(dashboard)/dashboard/schedules/loading.tsx
+++ src/app/(dashboard)/dashboard/schedules/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className=" size-full h-screen flex justify-center items-center">
+    <div className=" size-full h-[calc(100dvh-6rem)] flex justify-center items-center">
       <div className="loaderDot"></div>
     </div>
   );
