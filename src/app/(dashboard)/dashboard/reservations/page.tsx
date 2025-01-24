55547876926735aa362d04cec6c2a2134f75bad1
amir-shz <amirsharif2079@gmail.com>
2025-01-24 17:22:15 +0330

    test production

--- src/app/(dashboard)/dashboard/reservations/page.tsx
+++ src/app/(dashboard)/dashboard/reservations/page.tsx
@@ -1,26 +1,27 @@
-import ReservationHeader from "@/components/dashboard/ReservationHeader";
-import ReservationRow from "@/components/dashboard/ReservationRow";
-import { reservationType } from "@/types/types";
+// import ReservationHeader from "@/components/dashboard/ReservationHeader";
+// import ReservationRow from "@/components/dashboard/ReservationRow";
+// import { reservationType } from "@/types/types";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "رزروها",
 };
 
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+// const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 async function page() {
-  const reservations = await fetch(`${baseUrl}/reservation`)
-    .then((data) => data.json())
-    .then((data) => data.data);
+  // const reservations = await fetch(`${baseUrl}/reservation`)
+  //   .then((data) => data.json())
+  //   .then((data) => data.data);
 
   return (
-    <div className="w-full flex flex-col rounded-md bg-purple-50 shadow-shadow4 border border-purple-100 ">
-      <ReservationHeader />
-      {reservations.map((reservation: reservationType) => (
-        <ReservationRow key={reservation._id} reservation={reservation} />
-      ))}
-    </div>
+    <div>test</div>
+    // <div className="w-full flex flex-col rounded-md bg-purple-50 shadow-shadow4 border border-purple-100 ">
+    //   <ReservationHeader />
+    //   {reservations.map((reservation: reservationType) => (
+    //     <ReservationRow key={reservation._id} reservation={reservation} />
+    //   ))}
+    // </div>
   );
 }
 
