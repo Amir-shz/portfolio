cc1f8aa0607b503de8fe012a5f468c5ac2343a37
amir-shz <amirsharif2079@gmail.com>
2025-02-03 01:13:48 +0330

    add favicon and manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "فاطمه شفیعی",
+    short_name: "فاطمه شفیعی",
+    description: "فاطمه شادات شفیعی | روانشناس صنعتی و سازمانی",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#EFE6FD",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
