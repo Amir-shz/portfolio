7a395157c0704fa8d3362fcf9688a92f0f951af3
amir-shz <amirsharif2079@gmail.com>
2025-02-02 13:47:41 +0330

    create not-found ad error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,7 @@
 "use client"; // Error boundaries must be Client Components
 
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
@@ -10,23 +12,43 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
+    document.title = "با مشکل مواجه شدیم";
+
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-      <p>{error.message}</p>
-      <p>{error.digest}</p>
-    </div>
+    <>
+      <div className=" w-full h-dvh relative flex flex-col items-center justify-center overflow-hidden">
+        <div className=" absolute bottom-44 text-center w-full z-50  flex flex-col gap-2 max-sm:bottom-52 max-[430px]:bottom-60">
+          <p className="text-4xl font-semibold text-error-700  max-sm:text-2xl">
+            با مشکلی مواجه شدیم
+          </p>
+          <p className=" text-error-800 mx-2">{error.message}</p>
+          <p>{error.digest}</p>
+        </div>
+        <div className="absolute  bottom-28  z-50  max-sm:bottom-40 flex flex-row-reverse gap-4 items-center max-[430px]:bottom-40">
+          <Link
+            href="/"
+            className=" bg-purple-400 px-6 py-2 text-neutral-50 font-medium rounded hover:bg-purple-600 duration-300"
+          >
+            بازگشت به خانه
+          </Link>
+          <button
+            className=" bg-error-500 px-6 py-2 text-neutral-50 font-medium rounded hover:bg-error-700 duration-300"
+            onClick={() => reset()}
+          >
+            تلاش مجدد
+          </button>
+        </div>
+      </div>
+      <Image
+        src="/error.svg"
+        alt="error"
+        fill
+        className=" scale-[65%] -translate-y-24 max-sm:scale-75 max-sm:-translate-y-24"
+      />
+    </>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,32 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "صفحه مورد نظر يافت نشد",
 };
 function NotFound() {
-  return <div>پیدا نشد</div>;
+  return (
+    <>
+      <div className=" w-full h-dvh relative flex flex-col items-center justify-center overflow-hidden">
+        <p className=" absolute bottom-36 text-center w-full z-50 text-4xl font-semibold text-purple-600 max-sm:bottom-72 max-sm:text-2xl">
+          صفحه مورد نظر یافت نشد
+        </p>
+        <Link
+          href="/"
+          className=" bg-purple-400 px-6 py-2 text-neutral-50 font-medium rounded z-50 absolute bottom-20 hover:bg-purple-600 duration-300 max-sm:bottom-56"
+        >
+          بازگشت به خانه
+        </Link>
+      </div>
+      <Image
+        src="/404.svg"
+        alt="404"
+        fill
+        className=" scale-90 -translate-y-16 max-sm:scale-100 max-sm:-translate-y-24"
+      />
+    </>
+  );
 }
 
 export default NotFound;
