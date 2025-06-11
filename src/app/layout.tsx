e1bcbc994271616f39b49ab948f4a46afb1410f5
amir-shz <amirsharif2079@gmail.com>
2025-06-11 16:31:01 +0330

    add google analytics and google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { Viewport } from "next";
 import PWAInstallPrompt from "@/components/ui/PWAInstallPrompt";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 const Vazirmatn = localFont({
   src: [
@@ -66,9 +67,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DWMG1QLPHQ"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DWMG1QLPHQ');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T3QG8FJ9');
+          `}
+        </Script>
+      </head>
       <body
         className={`${Vazirmatn.variable} ${Vazirmatn.className} antialiased bg-background`}
       >
+        {/* GTM NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T3QG8FJ9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
 
         <PWAInstallPrompt />
